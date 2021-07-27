edb62bbf8a3f2115005c1dfd7408c2778f1bf99e
kitoshi <kitoshi.charlton@gmail.com>
2021-07-27 14:39:14 -0700

    removed users path

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/crawlerAPI", crawlerAPIrouter);
 app.use("/ebayAPI", ebayAPIrouter);
 
