b32ea13446162cedda347d6201eb5e2e8b9e85d8
kitoshi <kitoshi.charlton@gmail.com>
2021-08-06 12:15:33 -0700

    fixed routes bug, readme updates

--- routes/crawlerAPI.js
+++ routes/crawlerAPI.js
@@ -1,30 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', async function(req, res, next) {
-    const redis = require("redis");
-    const client = redis.createClient({
-        host: 'redis-10514.c60.us-west-1-2.ec2.cloud.redislabs.com',
-        port: 10514,
-        password: '7e9Ui1fX1YsdPy2vJyxI9vsV0Fb9qZ7J',
-    })
+router.get("/", async function (req, res, next) {
+  const redis = require("redis");
+  const client = redis.createClient({
+    host: "redis-10514.c60.us-west-1-2.ec2.cloud.redislabs.com",
+    port: 10514,
+    password: "7e9Ui1fX1YsdPy2vJyxI9vsV0Fb9qZ7J",
+  });
 
-    client.on('error', function (error) {
-        console.error(error)
-    })
-    client.get('link', function (error, result) {
-        if (error) {
-            console.log(error);
-            throw error;
-        }
-        console.log('GET result ->' + result);
-        res.send(result);
-    });
-    await new Promise((r) => {
-        setTimeout(r, 10000);
-    redis.RedisClient.quit()
-    })
-
-    })
+  client.on("error", function (error) {
+    console.error(error);
+  });
+  client.get("link", function (error, result) {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+    console.log("GET result ->" + result);
+    res.send(result);
+  });
+});
 
 module.exports = router;
