2da883d496c49057d0aa6acd50b9ee1cd730af1f
kitoshi <kitoshi.charlton@gmail.com>
2021-08-06 14:37:43 -0700

    more readme and removed personal data

--- routes/crawlerAPI.js
+++ routes/crawlerAPI.js
@@ -6,7 +6,7 @@ router.get("/", async function (req, res, next) {
   const client = redis.createClient({
     host: "redis-10514.c60.us-west-1-2.ec2.cloud.redislabs.com",
     port: 10514,
-    password: "7e9Ui1fX1YsdPy2vJyxI9vsV0Fb9qZ7J",
+    password: process.env.REDIS_PASSWORD,
   });
 
   client.on("error", function (error) {
